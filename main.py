662e5136c79cbdb9d82b45f3b0b28a81e82d3fd4
Lluís Salvat Niell <87786112+lluissalvat@users.noreply.github.com>
2022-06-01 14:33:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -13,55 +13,55 @@ inctax_thres1 = 12570 #20%
 inctax_thres2 = 50270 #40%
 inctax_thres3 = 150000 #45%
 
-def studentLoanPayment (yearly_income):
+def studentLoanPayment (yearly_income) :
   if yearly_income < ug_thres :
     student_loan_payment = 0
   else :
     student_loan_payment = (yearly_income - ug_thres) * 0.09
   return student_loan_payment
 
-def incomeTaxPayment (yearly_income):
-  if yearly_income < inctax_thres1:
+def incomeTaxPayment (yearly_income) :
+  if yearly_income < inctax_thres1 :
     income_tax_payment = 0
-  elif yearly_income >= inctax_thres1 and yearly_income < inctax_thres2:
+  elif yearly_income >= inctax_thres1 and yearly_income < inctax_thres2 :
     income_tax_payment = (yearly_income - inctax_thres1) * 0.2
-  elif yearly_income >= inctax_thres2 and yearly_income < inctax_thres3:
+  elif yearly_income >= inctax_thres2 and yearly_income < inctax_thres3 :
     income_tax_payment = (inctax_thres2 - inctax_thres1) * 0.2 + (yearly_income - inctax_thres2) * 0.4
-  elif yearly_income >= inctax_thres3:
+  elif yearly_income >= inctax_thres3 :
     income_tax_payment = (inctax_thres2 - inctax_thres1) * 0.2 + (yearly_income - inctax_thres3) * 0.45 + (inctax_thres3 - inctax_thres2) * 0.4
   return income_tax_payment
 
-def nationalInsurancePayment (yearly_income):
-  if yearly_income < natins_thres1:
+def nationalInsurancePayment (yearly_income) :
+  if yearly_income < natins_thres1 :
     nat_ins_payment = 0
-  elif yearly_income >= natins_thres1 and yearly_income < natins_thres2:
+  elif yearly_income >= natins_thres1 and yearly_income < natins_thres2 :
     nat_ins_payment = (yearly_income - natins_thres1) * 0.12
-  elif yearly_income >= natins_thres2:
+  elif yearly_income >= natins_thres2 :
     nat_ins_payment = (natins_thres2 - natins_thres1) * 0.12 + (yearly_income - natins_thres2) * 0.02
   return nat_ins_payment
 
-def totalTaxPayment (yearly_income):
+def totalTaxPayment (yearly_income) :
   total_tax_payment = incomeTaxPayment(yearly_income) + nationalInsurancePayment(yearly_income) + studentLoanPayment(yearly_income)
   return total_tax_payment
 
-def effectiveTaxRate (yearly_income):
-  tax_rate = totalTaxPayment(yearly_income) / yearly_income *100
+def effectiveTaxRate (yearly_income) :
+  tax_rate = totalTaxPayment(yearly_income) / yearly_income * 100
   return tax_rate
 
-def netIncome (yearly_income):
+def netIncome (yearly_income) :
   net_income = yearly_income - totalTaxPayment(yearly_income)
   return net_income
 
-def annualSavings (yearly_income):
+def annualSavings (yearly_income) :
   annual_savings = netIncome(yearly_income) - expenses
   return annual_savings
 
-def monthlySavings (yearly_income):
+def monthlySavings (yearly_income) :
   monthly_savings = annualSavings(yearly_income) / 12
   return monthly_savings
 
-def netSavingsRate (yearly_income):
-  net_savings_rate = annualSavings(yearly_income) / netIncome(yearly_income) *100
+def netSavingsRate (yearly_income) :
+  net_savings_rate = annualSavings(yearly_income) / netIncome(yearly_income) * 100
   return net_savings_rate
 
 print('')
